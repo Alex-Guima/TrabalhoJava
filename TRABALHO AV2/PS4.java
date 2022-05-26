f31110ff388294f491c88b7121c667724d33120d
Lxndr <82479933+Alex-Guima@users.noreply.github.com>
2022-05-26 20:49:07 -0300

    Delete TRABALHO AV2 directory

--- TRABALHO AV2/PS4.java
+++
@@ -1,6 +0,0 @@
-public class PS4 extends Videogames{
-	public void consoles(){
-		System.out.printf("PS4: %n  Marca: %s%n  Principais Exclusivos: %s%n  Numero de Vendas: %s%n", getMarca(), getExclusivos(),getVendas());
-		System.out.println("-------------------------------------------------------");
-	}
-}
--- TRABALHO AV2/Xbox.java
+++
@@ -1,7 +0,0 @@
-public class Xbox extends Videogames{
-	public void consoles(){
-		System.out.printf("Xbox One: %n  Marca: %s%n  Principais Exclusivos: %s%n  Numero de Vendas %s%n", getMarca(),getExclusivos(),getVendas());
-		System.out.println("-------------------------------------------------------");
-	}
-}
-		
