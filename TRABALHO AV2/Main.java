8ae05297d6ae0a77f59010f90f31f96c3c28e140
Lxndr <contavs58@hotmail.com>
2022-05-27 08:33:03 -0300

    Usando o set no objeto criado da classe NSW

--- TRABALHO AV2/Main.java
+++ TRABALHO AV2/Main.java
@@ -3,6 +3,7 @@ public class Main{
 		Videogames videogame = new Videogames();
 		PS4 ps4 = new PS4();
 		Xbox xboxOne = new Xbox();
+		NSW nsw = new NSW();
 
 		ps4.setMarca("Sony");
 		ps4.setExclusivos("Bloodborne, Uncharted 4, God of War, Gran Turismo");
@@ -12,8 +13,13 @@ public class Main{
 		xboxOne.setExclusivos("Jogos Forza, Halo, Gears of War"); 
 		xboxOne.setVendas("56.000.000");
 
+		nsw.setMarca("Nintendo");
+		nsw.setExclusivos("Breath of the Wild, Super Mario Odyssey, Super Smash Bros Ultimate");
+		nsw.setVendas("107.000.000");
+
 		videogame.consoles();
 		ps4.consoles();
 		xboxOne.consoles();
+		nsw.consoles();
 	}
 }
