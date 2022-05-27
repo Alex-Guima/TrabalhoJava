a72736183b53d05ea1b02817d0a51c7628df655c
Lxndr <82479933+Alex-Guima@users.noreply.github.com>
2022-05-26 21:03:22 -0300

    Add classe NSW

---
+++ TRABALHO AV2/NSW.java
@@ -0,0 +1,6 @@
+public class NSW extends Videogames{
+	public void consoles(){
+		System.out.printf("PS4: %n  Marca: %s%n  Principais Exclusivos: %s%n  Numero de Vendas: %s%n", getMarca(), getExclusivos(),getVendas());
+		System.out.println("-------------------------------------------------------");
+	}
+}
