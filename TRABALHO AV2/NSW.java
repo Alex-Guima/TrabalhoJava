e5126330c86eb148fa2a8ebf07f73e92800f6c41
Lxndr <contavs58@hotmail.com>
2022-05-27 22:44:44 -0300

    Criando método construtor

--- TRABALHO AV2/NSW.java
+++ TRABALHO AV2/NSW.java
@@ -1,6 +1,6 @@
 public class NSW extends Videogames{
 	public void consoles(){
-		System.out.printf("PS4: %n  Marca: %s%n  Principais Exclusivos: %s%n  Numero de Vendas: %s%n", getMarca(), getExclusivos(),getVendas());
+		System.out.printf("Nintendo Switch: %n  Marca: %s%n  Principais Exclusivos: %s%n  Numero de Vendas: %s%n", getMarca(), getExclusivos(),getVendas());
 		System.out.println("-------------------------------------------------------");
 	}
 }
